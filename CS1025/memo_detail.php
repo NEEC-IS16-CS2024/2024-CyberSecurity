2e4997768100e564bbdd49937bb7656fd65a2aba
Awazon <61077801+Awazon@users.noreply.github.com>
2024-12-20 12:31:45 +0900

    可読性の向上 メモidを指定しなかった時の処理を追加 パラメータをurlencodeでエンコードして渡す

--- CS1025/memo_detail.php
+++ CS1025/memo_detail.php
@@ -5,20 +5,33 @@ require 'config.php';
 // セッションを開始
 session_start();
 
-// ログイン認証
-if (isset($_SESSION['user_id'])) {
-
-    // メモIDをGETパラメータから取得
-    $id = $_GET['id'];
-
-    // データベースからメモ情報を取得
-    $stmt = $pdo->prepare("SELECT * FROM posts WHERE post_id = ?");
-    $stmt->execute([$id]);
-    $memo = $stmt->fetch();
-
-} else {
+// ログインチェック
+if (!isset($_SESSION['user_id'])) {
     echo "ログインしていません<br />";
     echo '<a href="../CS1025/index.php">ログイン画面へ</a>';
+    exit;
+}
+
+// メモIDをGETパラメータから取得
+$id = $_GET['id'] ?? null;
+
+// メモIDが指定されていない場合の処理
+if (!$id) {
+    echo "メモIDが指定されていません。<br />";
+    echo '<a href="home.php">ホームへ戻る</a>';
+    exit;
+}
+
+// データベースからメモ情報を取得
+$stmt = $pdo->prepare("SELECT * FROM posts WHERE post_id = ?");
+$stmt->execute([$id]);
+$memo = $stmt->fetch();
+
+// メモが見つからない場合の処理
+if (!$memo) {
+    echo "指定されたメモが見つかりません。<br />";
+    echo '<a href="home.php">ホームへ戻る</a>';
+    exit;
 }
 ?>
 
@@ -28,18 +41,16 @@ if (isset($_SESSION['user_id'])) {
 
 <head>
     <meta charset="UTF-8">
-    <title>メモ一覧</title>
+    <title>メモ詳細</title>
 </head>
 
 <body>
-    <?php if (isset($_SESSION['user_id'])): ?>
-        <h2><?= htmlspecialchars($memo['post_title']) ?></h2>
-        <p>作成日: <?= htmlspecialchars($memo['created_at']) ?></p>
-        <p><?= nl2br(htmlspecialchars($memo['post_text'])) ?></p>
-        <a href="memo_edit.php?id=<?= $memo['post_id'] ?>">編集</a>
-        <a href="memo_delete_confirm.php?id=<?= $memo['post_id'] ?>">削除</a>
-        <a href="home.php">ホームへ戻る</a>
-    <?php endif ?>
+    <h2><?= htmlspecialchars($memo['post_title'], ENT_QUOTES, 'UTF-8') ?></h2>
+    <p>作成日: <?= htmlspecialchars($memo['created_at'], ENT_QUOTES, 'UTF-8') ?></p>
+    <p><?= nl2br(htmlspecialchars($memo['post_text'], ENT_QUOTES, 'UTF-8')) ?></p>
+    <a href="memo_edit.php?id=<?= urlencode($memo['post_id']) ?>">編集</a>
+    <a href="memo_delete_confirm.php?id=<?= urlencode($memo['post_id']) ?>">削除</a>
+    <a href="home.php">ホームへ戻る</a>
 </body>
 
 </html>
